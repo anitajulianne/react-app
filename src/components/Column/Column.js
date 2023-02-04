2bc524abdea676fba231459d9be11e968db4faea
Anita Zak <anita.zak01@gmail.com>
2023-02-05 00:34:15 +0100

    Modify reducer, Column, ColumnForm, CardForm

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -14,7 +14,7 @@ const Column = props => {
                 {cards.map(card => 
                 <Card key={card.id} {...card}/>)}
             </ul>
-            <CardForm columnId={props.id} action={props.addCard} />
+            <CardForm columnId={props.id} />
         </article>
     );
 }
